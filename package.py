f9b11ec7e3f921e48b1180892f038751d5db8b73
John Maximilian <2e0byo@gmail.com>
2021-06-02 22:18:41 +0100

    debugging

--- package.py
+++ package.py
@@ -5,7 +5,5 @@ from zipfile import ZipFile
 with ZipFile("pdfs.zip", "w") as zipf:
     for pdf in Path("vespers/").glob("**/*.pdf"):
         zipf.write(pdf, arcname=f"{pdf.parent.name}.pdf")
-
-from os import get_cwd
-print("Working dir is", get_cwd)
-
+print(list(Path(".").glob("*")))
+print(Path(".").resolve())
