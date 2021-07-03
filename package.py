482f2f5f781fb14d524dc928bc0e6c426b865850
John Maximilian <2e0byo@gmail.com>
2021-07-03 19:27:44 +0100

    add booklet

--- package.py
+++ package.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 from pathlib import Path
-from zipfile import ZipFile
 
 Path("./pdfs").mkdir()
 
 for pdf in Path("vespers/").glob("**/*.pdf"):
-    with Path(f"./pdfs/{pdf.parent.name}.pdf").open("wb") as f:
+    if pdf.stem.endswith("booklet"):
+        target = Path(f"./pdfs/{pdf.parent.name}-booklet.pdf")
+    else:
+        target = Path(f"./pdfs/{pdf.parent.name}.pdf")
+
+    # yeah, we could use copy...
+    with target.open("wb") as f:
         with pdf.open("rb") as g:
             f.write(g.read())
